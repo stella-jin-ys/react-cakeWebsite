0781c954c8da1bd2efa2b86667fc4e102b9fd587
Stella <stella.jin123@gmail.com>
2025-01-09 22:17:56 +0100

    update header

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,20 +3,20 @@ import { Link } from "react-scroll";
 
 export default function Header() {
   return (
-    <div className="flex justify-between bg-teal-300 px-20 py-5 text-teal-800">
+    <div className="flex justify-between  px-20 py-5 text-teal-800">
       <ul className="flex gap-20 cursor-pointer">
-        <li className="hover:text-white">
+        <li className="hover:border-b border-teal-600 text-teal-600">
           <Link to="home" smooth>
             Home
           </Link>
         </li>
-        <li className="hover:text-white">
+        <li className="hover:border-b border-teal-600 text-teal-600">
           <Link to="about">About</Link>
         </li>
-        <li className="hover:text-white">
+        <li className="hover:border-b border-teal-600 text-teal-600">
           <Link to="products">Products</Link>
         </li>
-        <li className="hover:text-white">
+        <li className="hover:border-b border-teal-600 text-teal-600">
           <Link to="contact">Contact</Link>
         </li>
       </ul>
